d88d67352357b224225c333a8489c596a490084b
Gurjit Kaur <kaurgurjit360@gmail.com>
2020-04-21 16:14:00 -0400

    Gurjit- Worked on adding background on landing page

--- insta-list/src/App.js
+++ insta-list/src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 import LandingPage from './Pages/LandingPage.js';
 
+
 function App() {
   return (
     <div className="App">
+            <div className="App-Header"> This is a header test</div>
       <LandingPage />
     </div>
   );
--- insta-list/src/Pages/LandingPage.js
+++ insta-list/src/Pages/LandingPage.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import MenuAppBar from './Components/navbar.js';
 import Artists from '../Artists.js';
+import background_image from './pictures/background_image.jpg';
 
+var sectionStyle = {
+  width: "100%",
+  height: "auto",
+};
+
+ 
 class LandingPage extends Component {
     render() {
         return (
             <div>
-            <MenuAppBar />
-            <div style={{color: "red"}}> here is a random list of Artists from DB: <Artists /> </div>
+                <MenuAppBar />
+                    <div style={{color: "red"}}> here is a random list of Artists from DB: <Artists />  </div>
+                    <img style= {sectionStyle} src={background_image}/>
             </div>
         );
     }
