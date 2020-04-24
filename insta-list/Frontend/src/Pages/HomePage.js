b9e25dc8c57228bd1a99b9b2c4502fc2de107066
Gurjit Kaur <kaurgurjit360@gmail.com>
2020-04-24 18:28:48 -0400

    Search changes with code explainations

--- insta-list/Frontend/src/Pages/HomePage.js
+++ insta-list/Frontend/src/Pages/HomePage.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
-import { Button } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
-import { FormControl } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
+import { DropdownButton } from 'react-bootstrap';
 import { ListGroup } from 'react-bootstrap';
 
 
@@ -10,62 +10,52 @@ class HomePage extends Component {
             super()
             this.state = {
                 search: "",
-                songs: [
-
-                    {id: 1, name: "New York" },
-                    {id: 2, name: "New York" },
-                    {id: 3, name: "No Idea" },
-                    {id: 4, name: "New" }
-
-                
-                ]
+                songs: []
             }
-            // this.handleChange = this.handleChange.bind(this)
         }
+    //handleChange detects any change in the form; anything that happens inside the form is an event. we are setting the value from inside the form (event.target.value) to state of search (which was created above).
     handleChange =  (event) => {
         this.setState({
             search: event.target.value
         });
     }
-
+    //this is checking, if a user typed anything the search form. the array result will check if the song names from our song table are EQUAL to what is typed in the search. It will return a filtered array of those results
     findSongs = () => {
+        if(this.state.search) { 
         let result = this.state.songs.filter(song => {
-        return song.name.includes(this.state.search)
-            
-        })
-        console.log(result)
-        // console.log(array)
-    
+         return song.song_name.includes(this.state.search) 
+            })
+           return result
+        }
     }
-
-
-
-
+    //this is feteching all the songs from the db and giving was a JSON form of them. Then its setting the state of the songs to whatever is in the DB
     componentDidMount() {
-        // fetch()
+        fetch("http://localhost:3000/api/allsong") 
+        .then(res => res.json())
+        .then(data => this.setState({ songs: data }))
     }
-
 
     render() {
         return (
-            <div>
-        <br/>
-        {/* <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-primary">Search</Button>
-        </Form> */}
-        <form>
-            <input type= "text" value={this.state.search} placeholder="Search" onChange={this.handleChange}/>
-             <h1> {this.state.search}</h1>
-             <Button variant="outline-primary" onClick={this.findSongs}>Search</Button>
-        </form>
-
-        <h4> See all songs </h4>
+        <div>
+         <br/>
+         <Form.Group>
+                <Form.Control value ={this.state.search} onChange={this.handleChange} size="lg" type="text" placeholder="Search Songs" />
+        </Form.Group>
+        {/* Below we are mapping through the filtered songs and as we map through them, we are adding them to the LIST. Next to each item in the list we add a dropdown button */} 
         <ListGroup>
-                <ListGroup.Item>Song1</ListGroup.Item>
-                <ListGroup.Item>Song2</ListGroup.Item>
+            {
+                this.findSongs() ? 
+               this.findSongs().map(song => {
+               return  <ListGroup.Item>{song.song_name } { <DropdownButton style= {{float: "right"}} id="dropdown-basic-button" title="Add to Playlist">
+                                                          <Dropdown.Item href="#/action-1">Playlist 1</Dropdown.Item>
+                                                          <Dropdown.Item href="#/action-2">Playlist 2</Dropdown.Item>
+                                                         </DropdownButton>}
+                        </ListGroup.Item>
+               }) : <strong >SEARCH A SONG</strong>
+            }   
         </ListGroup>
-        <h4> See all artists </h4>
+        {/* <h4> See all artists </h4>
         <ListGroup>
                 <ListGroup.Item>artist1</ListGroup.Item>
                 <ListGroup.Item>artist2</ListGroup.Item>
@@ -79,7 +69,7 @@ class HomePage extends Component {
         <ListGroup>
                 <ListGroup.Item>genres1</ListGroup.Item>
                 <ListGroup.Item>genres2</ListGroup.Item>
-        </ListGroup>  
+        </ListGroup>   */}
         </div>
         );
     }
